73bfdf843dd2f81cef9a93d77be68a577a800531
Carmen <carmen@oursky.com>
2017-03-15 16:58:27 +0800

    Set custom repeat background for chat view.

--- Applozic/ViewControllers/ALChatViewController.m
+++ Applozic/ViewControllers/ALChatViewController.m
@@ -2120,6 +2120,8 @@
     }
     
     self.refresh = YES;
+
+    [self setCustomRepeatBackgroundWallpaper];
     [self setBackGroundWallpaper];
 }
 
@@ -2140,6 +2142,14 @@
     UIImageView * backgroundImageView = [[UIImageView alloc] initWithFrame:self.view.frame];
     backgroundImageView.image = backgroundImage;
     [self.view insertSubview:backgroundImageView atIndex:0];
+}
+
+- (void)setCustomRepeatBackgroundWallpaper
+{
+    [self.mTableView setBackgroundColor:[UIColor clearColor]];
+    UIView *bgView = [[UIView alloc] init];
+    bgView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"pattern_bg"]];
+    self.mTableView.backgroundView = bgView;
 }
 
 //==============================================================================================================================================
