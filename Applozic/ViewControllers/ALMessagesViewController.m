b24ba906590d26311741f4ccf9a5c442432f365f
Carmen <carmen@oursky.com>
2017-03-24 13:45:33 +0800

    Custom contacts button style in message list view.

--- Applozic/ViewControllers/ALMessagesViewController.m
+++ Applozic/ViewControllers/ALMessagesViewController.m
@@ -138,6 +138,11 @@
         [self createAndLaunchChatView ];
     }
     
+    // Custom right item style
+    UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose
+                                                                                   target:self
+                                                                                   action:@selector(navigationRightButtonAction:)];
+    [self.navigationItem setRightBarButtonItem: barButtonItem];
 }
 
 -(void)loadMessages:(NSNotification *)notification
